1a58e1842b51128be721600c05129792bf37a177
Dot Click LLC <129838150+Dot-Click@users.noreply.github.com>
2025-03-06 04:03:39 +0500

    Add contract-related fields to Order model and migration script

---
+++ prisma/migrations/20250305225639_add_contract/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "Order" ADD COLUMN     "desc_contract" TEXT,
+ADD COLUMN     "desc_permission_to_build" TEXT,
+ADD COLUMN     "desc_pos" TEXT,
+ADD COLUMN     "desc_psc" TEXT;
