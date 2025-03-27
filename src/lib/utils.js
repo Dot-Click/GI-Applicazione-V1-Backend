edbe88352253db3b2f369aa90ede569a3a6eb6c0
Dot Click LLC <129838150+Dot-Click@users.noreply.github.com>
2025-03-28 02:43:46 +0500

    Change date format in formatDate function to DD/MM/YYYY structure for consistency

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -53,7 +53,7 @@ function formatDate(date) {
   const day = String(d.getDate()).padStart(2, "0");
   const month = String(d.getMonth() + 1).padStart(2, "0");
   const year = d.getFullYear();
-  return `${month}/${day}/${year}`;
+  return `${day}/${month}/${year}`;
 }
 
 /**
