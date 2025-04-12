7738853e0cf6fa0680071d88807071445cab6fad
Dot Click LLC <129838150+Dot-Click@users.noreply.github.com>
2025-04-12 20:04:40 +0500

    Add duration logging to logger middleware and update logger format

--- src/middlewares/logger.middleware.js
+++ src/middlewares/logger.middleware.js
@@ -3,19 +3,23 @@ import logger from "../lib/logger.js";
 export const loggerMiddleware = async (req, res, next) => {
     let oldJson = res.json; 
     let responseBody;
-  
+    const startHrTime = process.hrtime();
+    
     res.json = function (data) {
       responseBody = data;
       return oldJson.apply(res, arguments);
     };
 
   res.on("finish", () => {
+      const elapsedHrTime = process.hrtime(startHrTime);
+      const elapsedMs = (elapsedHrTime[0] * 1000 + elapsedHrTime[1] / 1e6).toFixed(3);
     if (res.statusCode >= 400) {
       logger.error({
         method: req.method,
         url: req.url,
         date: new Date().toDateString(),
         message: responseBody?.message || "Unknown error",
+        duration: elapsedMs
       });
     } else {
       logger.info({
@@ -23,6 +27,7 @@ export const loggerMiddleware = async (req, res, next) => {
         url: req.url,
         date: new Date().toDateString(),
         message: responseBody?.message || "Request received",
+        duration: elapsedMs
       });
     }
   });
