65166365100d20e44201b77af7382b13949edd5e
Dot Click LLC <129838150+Dot-Click@users.noreply.github.com>
2025-05-08 06:43:01 +0500

    Add createRicavi and createCosti functionalities: implement routes and controller logic for managing Ricavi and Costi, including required fields validation and database interactions.

--- src/controllers/fatture.controller.js
+++ src/controllers/fatture.controller.js
@@ -1,5 +1,5 @@
 import prisma from "../../prisma/prisma.js";
-import { cloudinaryUploader } from "../lib/utils.js";
+import { cloudinaryUploader, formatDate } from "../lib/utils.js";
 
 export const createFattureActive = async (req, res) => {
   try {
@@ -28,6 +28,7 @@ export const createFattureActive = async (req, res) => {
       "docDate",
       "vatRate",
       "split",
+      "docNo",
       "typology",
       "yearOfCompetence",
       "protocol",
@@ -98,29 +99,171 @@ export const getAllFattureActive = async (req, res) => {
 };
 
 export const getFattureActive = async (req, res) => {
-    try {
-      const { id } = req.params;
-  
-      const activeFatture = await prisma.invoice.findUnique({
-        where: { id },
-        include: { Customer: { select: { companyName: true } } },
-      });
-  
-      if (!activeFatture) {
-        return res.status(404).json({ message: "not found" });
-      }
-  
-      const { Customer, ...rest } = activeFatture;
-      const result = {
-        ...rest,
-        customerName: Customer?.companyName || null,
-      };
-  
-      return res.status(200).json({ message: "found", data: result });
-    } catch (error) {
-      return res.status(500).json({ message: error.message });
+  try {
+    const { id } = req.params;
+
+    const activeFatture = await prisma.invoice.findUnique({
+      where: { id },
+      include: { Customer: { select: { companyName: true } } },
+    });
+
+    if (!activeFatture) {
+      return res.status(404).json({ message: "not found" });
     }
+
+    const { Customer, ...rest } = activeFatture;
+    const result = {
+      ...rest,
+      customerName: Customer?.companyName || null,
+    };
+
+    return res.status(200).json({ message: "found", data: result });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
 };
 
+export const createRicavi = async (req, res) => {
+  try {
+    const {id} = req.user
+    const requiredFields = [
+      "customerName",
+      "wbs",
+      "iva",
+      "workSite",
+      "note",
+      "docDate",
+      "docNo",
+      "yearOfCompetence",
+      "revAmt",
+      "advancePayment",
+      "withHoldAmt",
+    ];
+    const {
+      customerName,
+      iva,
+      workSite,
+      docDate,
+      yearOfCompetence,
+      revAmt,
+      note,
+      docNo,
+      advancePayment,
+      withHoldAmt,
+      wbs,
+    } = req.body;
+    const missingField = requiredFields.find((field) => !req.body[field]);
+    if (missingField) {
+      return res
+        .status(400)
+        .json({ message: `Missing required field: ${missingField}` });
+    }
+    const ricavi = await prisma.ricavi.create({
+      data: {
+        iva,
+        workSite,
+        docDate: new Date(docDate),
+        yearOfCompetence,
+        revAmt,
+        advancePayment,
+        docNo,
+        note,
+        withHoldAmt,
+        wbs,
+        admin:{
+          connect: { id }
+        },
+        Customer: {
+          connect: { companyName: customerName },
+        },
+      },
+      include:{Customer:{select:{account:true}}}
+    });
+    return res.status(200).json({ message: "created!", data: ricavi });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
 
-  
+export const createCosti = async (req, res) => {
+  const {id} = req.user
+  try {
+    const requiredFields = [
+      "supplierName",
+      "wbs",
+      "iva",
+      "workSite",
+      "note",
+      "docDate",
+      "docNo",
+      "yearOfCompetence",
+      "revAmt",
+      "advancePayment",
+      "withHoldAmt",
+    ];
+    const {
+      supplierName,
+      iva,
+      workSite,
+      docDate,
+      yearOfCompetence,
+      revAmt,
+      docNo,
+      note,
+      advancePayment,
+      withHoldAmt,
+      wbs,
+    } = req.body;
+    const missingField = requiredFields.find((field) => !req.body[field]);
+    if (missingField) {
+      return res
+        .status(400)
+        .json({ message: `Missing required field: ${missingField}` });
+    }
+    const costi = await prisma.costi.create({
+      data: {
+        iva,
+        workSite,
+        docDate: new Date(docDate),
+        yearOfCompetence,
+        revAmt,
+        docNo,
+        advancePayment,
+        note,
+        withHoldAmt,
+        wbs,
+        admin:{
+          connect: { id }
+        },
+        supplier: {
+          connect: { companyName: supplierName },
+        },
+      },
+      include:{supplier:{select:{invoices:true}}}
+    });
+    return res.status(200).json({ message: "created!", data: costi });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+}
+
+export const getAllRicavis = async (req, res)=> {
+  try {
+    const {id} = req.user
+    let ricavis = await prisma.ricavi.findMany({where:{adminId:id},include:{Customer:{include:{invoices:{select:{docDate:true,docNo:true}}}}}})
+    ricavis = ricavis.map((obj)=>({...obj, customerName:obj.Customer.companyName,invoice:obj.Customer.invoices})).map(({ Customer, ...rest }) => rest);
+    return res.status(200).json({message:"fetched!", data:ricavis})
+  } catch (error) {
+    return res.status(500).json({message:error.message})
+  }
+}
+export const getAllCostis = async (req, res)=> {
+  try {
+    const {id} = req.user
+    let costis = await prisma.costi.findMany({where:{adminId:id},include:{supplier:{include:{invoices:{select:{docDate:true,docNo:true}}}}}})
+    costis = costis.map((obj)=>({...obj,supplierName: obj.supplier.companyName,invoice:obj.supplier.invoices,docDate:formatDate(obj.docDate)})).map(({ supplier, ...rest }) => rest);
+    return res.status(200).json({message:"fetched!", data:costis})
+  } catch (error) {
+    return res.status(500).json({message:error.message})
+  }
+}
--- src/routes/fatture.routes.js
+++ src/routes/fatture.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { createFattureActive, getAllFattureActive, getFattureActive } from "../controllers/fatture.controller.js";
+import { createCosti, createFattureActive, createRicavi, getAllCostis, getAllFattureActive, getAllRicavis, getFattureActive } from "../controllers/fatture.controller.js";
 import { Auth } from "../middlewares/auth.middleware.js";
 import { checkRole } from "../middlewares/verif.middleware.js";
 import { singleUpload } from "../middlewares/multer.middleware.js";
@@ -9,5 +9,9 @@ const router = Router()
 router.post("/create",Auth, checkRole(["ADMIN"]),singleUpload("active"),createFattureActive)
 router.get("/active",Auth, checkRole(["ADMIN"]), getAllFattureActive)
 router.get("/active/:id",Auth, checkRole(["ADMIN"]), getFattureActive)
+router.get("/ricavis",Auth,checkRole(["ADMIN"]), getAllRicavis)
+router.get("/costis",Auth,checkRole(["ADMIN"]), getAllCostis)
+router.post("/create/ricavi", Auth, checkRole(["ADMIN"]), createRicavi)
+router.post("/create/costi", Auth, checkRole(["ADMIN"]), createCosti)
 
 export default router
